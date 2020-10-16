840014608f1fe27cfe0d81fe569c2be2aeccab3c
Enderlook <enderlook@gmail.com>
2020-10-16 12:35:49 -0300

    Add Graph<TCoord, TSpatialIndex>, Graph3D, NodeId and Vector3Tree<TValue>, and allow unsafe code

---
+++ Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Pathfinding/Runtime/Graph/Graph`2.cs
@@ -0,0 +1,134 @@
+﻿using Enderlook.Collections;
+
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Pathfinding
+{
+    [Serializable]
+    public abstract class Graph<TCoord, TSpatialIndex> : IGraphIntrinsic<NodeId, IReadOnlyList<NodeId>>, ISerializationCallbackReceiver
+        where TSpatialIndex : ISpatialIndex<TCoord, int>, new()
+    {
+        [SerializeField]
+        private NodeInner[] nodes;
+
+        private TSpatialIndex tree;
+
+        /// <inheritdoc cref="IGraphIntrinsic{TNode}.GetCost(TNode, TNode)"/>
+        public abstract float GetCost(NodeId from, NodeId to);
+
+        /// <inheritdoc cref="IGraphIntrinsic{TNode, TNodes}.GetNeighbours(TNode)"/>
+        public IReadOnlyList<NodeId> GetNeighbours(NodeId node) => nodes[node.id].edges;
+
+        protected Node GetNode(NodeId node) => nodes[node.id].ToNode();
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            tree = new TSpatialIndex();
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                tree.Insert(nodes[i].position, i);
+            }
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+#if UNITY_EDITOR
+            // If we are in the editor, we can try and optimize nodes by sorting them.
+
+            int nodesLength = nodes.Length;
+            NodeClass[] nodesClass = new NodeClass[nodesLength];
+            for (int i = 0; i < 0; i++)
+                nodesClass[i] = new NodeClass() { position = nodes[i].position };
+
+            Dictionary<NodeClass, NodeId[]> oldEdges = new Dictionary<NodeClass, NodeId[]>(nodesLength);
+
+            for (int i = 0; i < nodesLength; i++)
+            {
+                NodeClass node = nodesClass[i];
+
+                NodeId[] edges_ = nodes[i].edges;
+                oldEdges[node] = edges_;
+
+                NodeClass[] edges = new NodeClass[edges_.Length];
+                for (int j = 0; j < edges_.Length; j++)
+                    edges[j] = nodesClass[edges_[j].id];
+                node.edges = edges;
+            }
+
+            Array.Sort(nodesClass);
+
+            Dictionary<NodeClass, int> index = new Dictionary<NodeClass, int>(nodesLength);
+            for (int i = 0; i < nodesLength; i++)
+            {
+                NodeClass node = nodesClass[i];
+                nodes[i].position = node.position;
+                index[node] = i;
+            }
+            for (int i = 0; i < nodesLength; i++)
+            {
+                NodeClass node = nodesClass[i];
+                NodeClass[] edges_ = node.edges;
+                NodeId[] edges = oldEdges[node];
+                for (int j = 0; j < edges_.Length; j++)
+                    edges[j] = new NodeId(index[edges_[j]]);
+                nodes[i].edges = edges;
+                Array.Sort(edges); // Not necessary
+            }
+#endif
+        }
+
+        /// <inheritdoc cref="IGraphIntrinsic{TNodeId}.GetNeighbours(TNodeId)"/>
+        IEnumerable<NodeId> IGraphIntrinsic<NodeId>.GetNeighbours(NodeId node) => GetNeighbours(node);
+
+        [Serializable]
+        /// <summary>
+        /// Represent an ephemereal node.
+        /// </summary>
+        private struct NodeInner
+        {
+            public TCoord position;
+
+            public NodeId[] edges;
+
+            public Node ToNode()
+            {
+                // TODO: Use MemoryMarshal
+                return new Node(position, edges);
+            }
+        }
+
+        /// <summary>
+        /// Represent an ephemereal node.
+        /// </summary>
+        protected readonly struct Node
+        {
+            /// <summary>
+            /// Position of the node.
+            /// </summary>
+            public readonly TCoord Position;
+
+            /// <summary>
+            /// Don't store this content outside of the node.
+            /// </summary>
+            public readonly IReadOnlyList<NodeId> Edges;
+
+            public Node(TCoord position, NodeId[] edges)
+            {
+                Position = position;
+                Edges = edges;
+            }
+        }
+
+#if UNITY_EDITOR
+        [Serializable]
+        private class NodeClass
+        {
+            public TCoord position;
+            public NodeClass[] edges;
+        }
+#endif
+    }
+}
---
+++ Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Pathfinding/Runtime/Structures/Vector3Tree`1.cs
@@ -0,0 +1,51 @@
+﻿using Enderlook.Collections;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Pathfinding
+{
+    /// <summary>
+    /// A wrapper arround <see cref="D3Tree{TKey, TValue}"/> to accept <see cref="Vector3"/>
+    /// </summary>
+    /// <typeparam name="TValue">Type of value.</typeparam>
+    public struct Vector3Tree<TValue> : ISpatialIndex<Vector3, TValue>
+        where TValue : unmanaged
+    {
+        // TODO: use MemoryMarshal in order to remove the unmanaged constrain to TValue
+
+        private readonly D3TreeFloat<TValue> tree;
+
+        /// <inheritdoc cref="ISpatialIndexBasic{TKey, TValue}.Contains(TKey)"/>
+        public bool Contains(Vector3 key) => tree.Contains(ToTuple(key));
+
+        /// <inheritdoc cref="ISpatialIndex{TKey, TValue}.FindNearestNeighbour(TKey)"/>
+        public (Vector3 key, TValue value, double distance) FindNearestNeighbour(Vector3 key) => FromTuple(tree.FindNearestNeighbour(ToTuple(key)));
+
+        /// <inheritdoc cref="ISpatialIndexBasic{TKey, TValue}.GetValue(TKey)"/>
+        public TValue GetValue(Vector3 key) => tree.GetValue(ToTuple(key));
+
+        /// <inheritdoc cref="ISpatialIndexBasic{TKey, TValue}.Insert(TKey, TValue)"/>
+        public void Insert(Vector3 key, TValue value) => tree.Insert(ToTuple(key), value);
+
+        /// <inheritdoc cref="ISpatialIndexBasic{TKey, TValue}.InsertOrUpdate(TKey, TValue)(TKey, TValue)"/>
+        public void InsertOrUpdate(Vector3 key, TValue value) => tree.InsertOrUpdate(ToTuple(key), value);
+
+        /// <inheritdoc cref="ISpatialIndexBasic{TKey, TValue}.Remove(TKey)"/>
+        public bool Remove(Vector3 key) => tree.Remove(ToTuple(key));
+
+        /// <inheritdoc cref="ISpatialIndexBasic{TKey, TValue}.TryGetValue(TKey, out TValue)"/>
+        public bool TryGetValue(Vector3 key, out TValue value) => tree.TryGetValue(ToTuple(key), out value);
+
+        private static unsafe (float, float, float) ToTuple(Vector3 vector) =>
+#pragma warning disable IDE0047 // Remove unnecessary parentheses
+            *((float, float, float) *)(&vector);
+#pragma warning restore IDE0047 // Remove unnecessary parentheses
+
+
+        private unsafe (Vector3 key, TValue value, double distance) FromTuple(((float, float, float) key, TValue value, double distance) value) =>
+#pragma warning disable IDE0047 // Remove unnecessary parentheses
+            *((Vector3, TValue, double) *)(&value);
+#pragma warning restore IDE0047 // Remove unnecessary parentheses
+
+    }
+}
