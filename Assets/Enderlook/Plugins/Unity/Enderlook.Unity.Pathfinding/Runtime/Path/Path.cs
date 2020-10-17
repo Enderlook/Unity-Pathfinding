2b8ff1b359f744d52f0722616926e5e67e2cce30
Enderlook <enderlook@gmail.com>
2020-10-16 22:58:24 -0300

    Implement IPathFeedable<IEnumerator<TNode>> in Path<TNode>

--- Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Pathfinding/Runtime/Path/Path.cs
+++ Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Pathfinding/Runtime/Path/Path.cs
@@ -6,7 +6,7 @@ namespace Enderlook.Unity.Pathfinding
     /// Represents a path.
     /// </summary>
     /// <typeparam name="TNode">Node type.</typeparam>
-    public sealed class Path<TNode> : IPathFeedable<IEnumerable<TNode>>, IPathFeedable<IReadOnlyList<TNode>>
+    public sealed class Path<TNode> : IPathFeedable<IEnumerable<TNode>>, IPathFeedable<IReadOnlyList<TNode>>, IPathFeedable<IEnumerator<TNode>>
     {
         private readonly List<TNode> nodes = new List<TNode>();
 
@@ -24,6 +24,14 @@ namespace Enderlook.Unity.Pathfinding
             nodes.Clear();
             // We don't check capacity for optimization because that is already done in AddRange.
             nodes.AddRange(information);
+        }
+
+        /// <inheritdoc cref="IPathFeedable{TInfo}"/>
+        public void Feed(IEnumerator<TNode> information)
+        {
+            nodes.Clear();
+            while (information.MoveNext())
+                nodes.Add(information.Current);
         }
     }
 }
