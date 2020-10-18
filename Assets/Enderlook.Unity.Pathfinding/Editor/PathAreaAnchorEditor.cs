66a1a008c12a7d43d507c2838ca918f1542ff9da
Enderlook <enderlook@gmail.com>
2020-10-18 16:13:24 -0300

    Add PathAreaAnchor and PathAreaAnchorEditor

---
+++ Assets/Enderlook.Unity.Pathfinding/Editor/PathAreaAnchorEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Pathfinding
+{
+    [CustomEditor(typeof(PathAreaAnchor))]
+    internal sealed class PathAreaAnchorEditor : Editor
+    {
+        private readonly static GUIContent BAKE_BUTTON = new GUIContent("Bake", "Bake the path area.");
+
+        private new PathAreaAnchor target;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void OnEnable() => target = (PathAreaAnchor)base.target;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            EditorGUI.BeginChangeCheck();
+            if (GUILayout.Button(BAKE_BUTTON))
+                target.Bake();
+            if (EditorGUI.EndChangeCheck())
+                serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Enderlook.Unity.Pathfinding/Runtime/Components/PathAreaAnchor.cs
@@ -0,0 +1,59 @@
+﻿using Enderlook.Unity.Attributes;
+
+using System;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Pathfinding
+{
+    [AddComponentMenu("Enderlook/Pathfinding/PathAreaManagerAnchor")]
+    public class PathAreaAnchor : MonoBehaviour
+    {
+#pragma warning disable CS0649
+        [SerializeField, Tooltip("Defines which GameObjects are used to determine the path area.")]
+        private CollectionType collectionType;
+
+        [SerializeField, ShowIf(nameof(collectionType), typeof(CollectionType), CollectionType.Volume, true), Tooltip("Objects in range contribute to the path area.")]
+        private float collectionSize;
+
+        [SerializeField, Tooltip("Define the layers of which GameObject are be included.")]
+        private LayerMask filterInclude;
+
+        [SerializeField, Tooltip("Determines the geometry used for path areas.")]
+        private GeometryType geometryType;
+
+        [SerializeField, ShowIf(nameof(geometryType), typeof(GeometryType), GeometryType.PhysicsColliders, true), Tooltip("Whenever it should include trigger colliders for path areas.")]
+        private bool includeTriggerColliders;
+
+        [SerializeField, Min(1), Tooltip("Defiens the level of detail of the path area.")]
+        private int subdivisions = 1;
+
+        [SerializeField, HideInInspector]
+        private Octree graph;
+#pragma warning restore CS0649
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void OnDrawGizmos()
+        {
+            if ((collectionType & CollectionType.Volume) == 0)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireCube(transform.position, Vector3.one * collectionSize * 2);
+            }
+
+            graph.DrawGizmos();
+        }
+
+        internal void Bake()
+        {
+            if (geometryType == GeometryType.RenderMeshes)
+                throw new NotImplementedException();
+
+            if (collectionType == CollectionType.Children)
+                throw new NotImplementedException();
+
+            graph.Reset(transform.position, collectionSize, subdivisions);
+            graph.SubdivideFromObstacles(filterInclude, includeTriggerColliders);
+        }
+    }
+}
