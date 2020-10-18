8fc30e561d2ca62951c03e4af6805fdcc3c9d2da
Enderlook <enderlook@gmail.com>
2020-10-18 10:38:23 -0300

    Add AgentType and AgentTypeEditor

---
+++ Assets/Enderlook.Unity.Pathfinding/Editor/AgentTypeEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Pathfinding
+{
+    [CustomEditor(typeof(AgentType))]
+    internal sealed class AgentTypeEditor : Editor
+    {
+        private static readonly GUIContent NAME_CONTENT = new GUIContent("Name", "Name of the agent type.");
+
+        private new AgentType target;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void OnEnable() => target = (AgentType)base.target;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            EditorGUI.BeginChangeCheck();
+            target.name = EditorGUILayout.TextField(NAME_CONTENT, target.name);
+            if (EditorGUI.EndChangeCheck())
+                serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Enderlook.Unity.Pathfinding/Runtime/Scriptable Objects/AgentType.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Enderlook.Unity.Pathfinding
+{
+    [CreateAssetMenu(fileName = "Agent Type", menuName = "Enderlook/Pathfinding/Agent Type")]
+    public class AgentType : ScriptableObject
+    {
+#pragma warning disable CS0649
+        [SerializeField, Tooltip("How close to the walls navigation mesh exists.")]
+        private float agentRadius = .5f;
+
+        [SerializeField, Tooltip("How much vertical clearance space must exist.")]
+        private float agentHeight = 2;
+
+        [SerializeField, Range(0, 60), Tooltip("Maximum slope the agent can walk up.")]
+        private float maxSlope = 45;
+
+        [SerializeField, Tooltip("The height of discontinuities in the level the agent can climb over. (i.e: steps and stairs)")]
+        private float stepHeight = .4f;
+#pragma warning restore CS0649
+    }
+}
