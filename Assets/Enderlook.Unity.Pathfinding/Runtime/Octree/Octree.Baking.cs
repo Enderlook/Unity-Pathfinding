4719029591761a65a6fd8a34ad079c1dbf6e2bc1
Enderlook <enderlook@gmail.com>
2020-10-21 22:53:38 -0300

    Reorganice code and remove Graph and Vector3Tree

--- Assets/Enderlook.Unity.Pathfinding/Runtime/Octree/Octree.Baking.cs
+++ Assets/Enderlook.Unity.Pathfinding/Runtime/Octree/Octree.Baking.cs
@@ -1,50 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Enderlook.Unity.Pathfinding
 {
     internal sealed partial class Octree
     {
-        [SerializeField]
-        private Vector3 center;
-
-        [SerializeField]
-        private float size;
-
-        [SerializeField]
-        private byte subdivisions;
-
-        private Dictionary<LocationCode, InnerOctant> octants;
-
-        internal int OctantsCount => octants.Count;
-
-        public Octree(Vector3 center, float size, byte subdivisions)
-        {
-            this.center = center;
-            this.size = size;
-            this.subdivisions = subdivisions;
-            if (subdivisions > 9)
-                throw new ArgumentOutOfRangeException(nameof(subdivisions), "Must be a value from 1 to 10.", subdivisions.ToString());
-        }
-
-        internal void Reset(Vector3 center, float size, byte subdivisions)
-        {
-            if (subdivisions > 9)
-                throw new ArgumentOutOfRangeException(nameof(subdivisions), "Must be a value from 1 to 10.", subdivisions.ToString());
-
-            octantsBytes = null;
-
-            this.center = center;
-            this.size = size;
-            this.subdivisions = subdivisions;
-
-            Clear();
-        }
-
-        private void Clear() => octants.Clear();
-
         internal void SubdivideFromObstacles(LayerMask filterInclude, bool includeTriggerColliders)
         {
             Clear();
@@ -64,8 +23,10 @@ namespace Enderlook.Unity.Pathfinding
             Vector3 center,
             ref (LayerMask filterInclude, QueryTriggerInteraction query, Collider[] test) tuple)
         {
-            InnerOctant octant = new InnerOctant(code, center);
-            octant.Center = center;
+            InnerOctant octant = new InnerOctant(code, center)
+            {
+                Center = center
+            };
 
             float currentSize = octant.GetSize(size) * .5f;
 
---
+++ Assets/Enderlook.Unity.Pathfinding/Runtime/Octree/Octree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Pathfinding
+{
+    internal sealed partial class Octree
+    {
+        [SerializeField]
+        private Vector3 center;
+
+        [SerializeField]
+        private float size;
+
+        [SerializeField]
+        private byte subdivisions;
+
+        private Dictionary<LocationCode, InnerOctant> octants;
+
+        internal int OctantsCount => octants.Count;
+
+        public Octree(Vector3 center, float size, byte subdivisions)
+        {
+            this.center = center;
+            this.size = size;
+            this.subdivisions = subdivisions;
+            if (subdivisions > 9)
+                throw new ArgumentOutOfRangeException(nameof(subdivisions), "Must be a value from 1 to 10.", subdivisions.ToString());
+        }
+
+        internal void Reset(Vector3 center, float size, byte subdivisions)
+        {
+            if (subdivisions > 9)
+                throw new ArgumentOutOfRangeException(nameof(subdivisions), "Must be a value from 1 to 10.", subdivisions.ToString());
+
+            octantsBytes = null;
+
+            this.center = center;
+            this.size = size;
+            this.subdivisions = subdivisions;
+
+            Clear();
+        }
+
+        private void Clear() => octants.Clear();
+    }
+}
