db965766edff9f0c23fe38abe550af1dbf967c8c
Enderlook <enderlook@gmail.com>
2020-10-23 12:51:20 -0300

    Implement IGraphIntrinsics<TNode, TNode> in Octree

--- Assets/Enderlook.Unity.Pathfinding/Runtime/Octree/Octree.OctantCode.cs
+++ Assets/Enderlook.Unity.Pathfinding/Runtime/Octree/Octree.OctantCode.cs
@@ -9,47 +9,48 @@ namespace Enderlook.Unity.Pathfinding
     internal sealed partial class Octree
     {
         [Serializable, System.Diagnostics.DebuggerDisplay("{Code}")]
-        private readonly struct OctantCode : IEquatable<OctantCode>, IComparable<OctantCode>
+        public readonly struct OctantCode : IEquatable<OctantCode>, IComparable<OctantCode>
         {
             public const uint DIL_X = 0b001_001_001_001_001_001_001_001_001_00000;
             public const uint DIL_Y = 0b010_010_010_010_010_010_010_010_010_00000;
             public const uint DIL_Z = 0b100_100_100_100_100_100_100_100_100_00000;
             public const uint DIL_XYZ = DIL_X | DIL_Y | DIL_Z;
 
-            public const int SIZE = sizeof(uint);
+            internal const int SIZE = sizeof(uint);
 
             // https://geidav.wordpress.com/2014/08/18/advanced-octrees-2-node-representations/
             // A Morton 3D Code with includes depth
 
-            public readonly uint Code;
+            internal readonly uint Code;
 
-            public int Depth {
+            internal int Depth {
                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
                 get => Mathf.FloorToInt(Mathf.Log(Code, 2) / 3);// Alternative this may work: (31 - System.Numerics.BitOperations.LeadingZeroCount(Code)) / 3;
             }
 
-            public OctantCode Parent {
+            internal OctantCode Parent {
                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
                 get => new OctantCode(Code >> 3);
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public OctantCode(uint code) => Code = code;
+            internal OctantCode(uint code) => Code = code;
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public OctantCode GetChildTh(uint th)
+            internal OctantCode GetChildTh(uint th)
             {
                 Debug.Assert(th <= 8, th);
                 return new OctantCode((Code << 3) | th);
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public OctantCode(ReadOnlySpan<byte> source) => Code = BinaryPrimitives.ReadUInt32LittleEndian(source);
+            internal OctantCode(ReadOnlySpan<byte> source) => Code = BinaryPrimitives.ReadUInt32LittleEndian(source);
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public bool IsMaxDepth(uint maxDepth) => Code == maxDepth;
+            internal bool IsMaxDepth(uint maxDepth) => Code == maxDepth;
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal float GetSize(float rootSize) => Mathf.Pow(.5f, Depth) * rootSize;
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public int CompareTo(OctantCode other) => Code.CompareTo(other.Code);
@@ -77,7 +78,7 @@ namespace Enderlook.Unity.Pathfinding
             public static bool operator !=(OctantCode a, OctantCode b) => !a.Equals(b);
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void WriteBytes(Span<byte> destination) => BinaryPrimitives.WriteUInt32LittleEndian(destination, Code);
+            internal void WriteBytes(Span<byte> destination) => BinaryPrimitives.WriteUInt32LittleEndian(destination, Code);
         }
     }
 }
