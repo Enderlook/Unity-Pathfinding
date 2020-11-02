00ab54db4e08fe72806f4f1d59cda9e8edde1daa
Enderlook <enderlook@gmail.com>
2020-11-02 20:20:07 -0300

    Reorganice ConvexHull class

---
+++ Assets/Enderlook.Unity.Pathfinding/Runtime/Navigation Mesh/ConvexHull.GrahamScan.cs
@@ -0,0 +1,51 @@
+﻿using NET5.System;
+
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Pathfinding
+{
+    internal static partial class ConvexHull
+    {
+        private static class GrahamScan
+        {
+            public static void CalculateConvexHull(Span<Vector2> points, List<Vector2> output)
+            {
+                if (output is null)
+                    throw new ArgumentNullException(nameof(output));
+
+                output.Clear();
+
+                int n = points.Length;
+                if (n <= 1)
+                    return;
+
+                Vector2 c = points[FindBottomostPoint(points)];
+                points.Sort(new Comparer(c));
+
+                int j = 0;
+                for (int i = 0; i < n; ++i)
+                    KeepLeft(output, 0, ref j, points[i]);
+
+                points.Reverse();
+
+                int k = 0;
+                for (int i = 0; i < n; ++i)
+                    KeepLeft(output, j, ref k, points[i]);
+            }
+
+            private static void KeepLeft(List<Vector2> v, int start, ref int index, Vector2 p)
+            {
+                while (index > 1 && GetOrientation(v[start + index - 2], v[start + index - 1], p) != OrientationType.LeftTurn)
+                    v.RemoveAt(start + index-- - 1);
+                if (index == 0 || v[start + index - 1] != p)
+                {
+                    v.Add(p);
+                    index++;
+                }
+            }
+        }
+    }
+}
--- Assets/Enderlook.Unity.Pathfinding/Runtime/Navigation Mesh/ConvexHull.cs
+++ Assets/Enderlook.Unity.Pathfinding/Runtime/Navigation Mesh/ConvexHull.cs
@@ -1,6 +1,4 @@
-﻿using NET5.System;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 using UnityEngine;
@@ -9,30 +7,8 @@ namespace Enderlook.Unity.Pathfinding
 {
     internal static partial class ConvexHull
     {
-        public static void GrahamScan(Span<Vector2> points, List<Vector2> output)
-        {
-            if (output is null)
-                throw new ArgumentNullException(nameof(output));
-
-            output.Clear();
-
-            int n = points.Length;
-            if (n <= 1)
-                return;
-
-            Vector2 c = points[FindBottomostPoint(points)];
-            points.Sort(new Comparer(c));
-
-            int j = 0;
-            for (int i = 0; i < n; ++i)
-                KeepLeft(output, 0, ref j, points[i]);
-
-            points.Reverse();
-
-            int k = 0;
-            for (int i = 0; i < n; ++i)
-                KeepLeft(output, j, ref k, points[i]);
-        }
+        public static void CalculateConvexHull(Span<Vector2> points, List<Vector2> output)
+            => GrahamScan.CalculateConvexHull(points, output);
 
         internal static int FindBottomostPoint(Span<Vector2> P)
         {
@@ -68,17 +44,6 @@ namespace Enderlook.Unity.Pathfinding
                 if (orientation == OrientationType.Collinear)
                     return (Vector2.Distance(p0, p2) >= Vector2.Distance(p0, p1)) ? -1 : 1;
                 return (orientation == OrientationType.CounterClockwise) ? -1 : 1;
-            }
-        }
-
-        private static void KeepLeft(List<Vector2> v, int start, ref int index, Vector2 p)
-        {
-            while (index > 1 && GetOrientation(v[start + index - 2], v[start + index - 1], p) != OrientationType.LeftTurn)
-                v.RemoveAt(start + index-- - 1);
-            if (index == 0 || v[start + index - 1] != p)
-            {
-                v.Add(p);
-                index++;
             }
         }
 
