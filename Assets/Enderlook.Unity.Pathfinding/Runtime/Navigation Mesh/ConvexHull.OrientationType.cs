b7c8e52c327b4c57d2d7ebaf6ee3634cbb1645a8
Enderlook <enderlook@gmail.com>
2020-11-02 18:55:52 -0300

    Refactor ConvexHull and create ConvexHull.OrientationType type

---
+++ Assets/Enderlook.Unity.Pathfinding/Runtime/Navigation Mesh/ConvexHull.OrientationType.cs
@@ -0,0 +1,14 @@
+﻿namespace Enderlook.Unity.Pathfinding
+{
+    internal static partial class ConvexHull
+    {
+        private enum OrientationType
+        {
+            Clockwise = -1,
+            RightTurn = Clockwise,
+            Collinear = 0,
+            CounterClockwise = 1,
+            LeftTurn = CounterClockwise,
+        }
+    }
+}
--- Assets/Enderlook.Unity.Pathfinding/Runtime/Navigation Mesh/ConvexHull.cs
+++ Assets/Enderlook.Unity.Pathfinding/Runtime/Navigation Mesh/ConvexHull.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 namespace Enderlook.Unity.Pathfinding
 {
-    internal static class ConvexHull
+    internal static partial class ConvexHull
     {
-        private const int LEFT_TURN = 1;
-        private const int COUNTER_CLOCKWISE = LEFT_TURN;
-        private const int RIGHT_TURN = -1;
-        private const int CLOCKWISE = RIGHT_TURN;
-        private const int COLLINEAR = 0;
-
         public static List<Vector2> GrahamScan(List<Vector2> points)
         {
             if (points.Count <= 1)
@@ -62,31 +56,31 @@ namespace Enderlook.Unity.Pathfinding
 
         private static int Compare(Vector2 p0, Vector2 p1, Vector2 p2)
         {
-            int orientation = Orientation(p0, p1, p2);
-            if (orientation == 0)
+            OrientationType orientation = GetOrientation(p0, p1, p2);
+            if (orientation == OrientationType.Collinear)
                 return (Vector2.Distance(p0, p2) >= Vector2.Distance(p0, p1)) ? -1 : 1;
-            if (orientation == 1)
+            if (orientation == OrientationType.CounterClockwise)
                 return -1;
             return 1;
         }
 
         private static List<Vector2> KeepLeft(List<Vector2> v, Vector2 p)
         {
-            while (v.Count > 1 && Orientation(v[v.Count - 2], v[v.Count - 1], p) != LEFT_TURN)
+            while (v.Count > 1 && GetOrientation(v[v.Count - 2], v[v.Count - 1], p) != OrientationType.LeftTurn)
                 v.RemoveAt(v.Count - 1);
             if (v.Count == 0 || v[v.Count - 1] != p)
                 v.Add(p);
             return v;
         }
 
-        private static int Orientation(Vector2 p0, Vector2 p1, Vector2 p2)
+        private static OrientationType GetOrientation(Vector2 p0, Vector2 p1, Vector2 p2)
         {
             float value = ((p1.y - p0.y) * (p2.x - p1.x)) - ((p1.x - p0.x) * (p2.y - p1.y));
             if (value == 0)
-                return COLLINEAR;
+                return OrientationType.Collinear;
             if (value > 0)
-                return CLOCKWISE;
-            return COUNTER_CLOCKWISE;
+                return OrientationType.Clockwise;
+            return OrientationType.CounterClockwise;
         }
     }
 }
