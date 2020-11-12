c2cdd5d28376aab42d6820a34a554e0c87eb8d11
Enderlook <enderlook@gmail.com>
2020-11-12 12:42:36 -0300

    Extract from NavigationAgent ObstacleAvoidance, and create ISteering

--- Assets/Enderlook.Unity.Pathfinding/Runtime/Navigation/NavigationAgent.cs
+++ Assets/Enderlook.Unity.Pathfinding/Runtime/Navigation/NavigationAgent.cs
@@ -1,8 +1,5 @@
 ﻿using Assets.Enderlook.Unity.Pathfinding;
-
-using Enderlook.Unity.Attributes;
-
-using System;
+using Assets.Enderlook.Unity.Pathfinding.Steerings;
 
 using UnityEngine;
 
@@ -44,45 +41,14 @@ namespace Enderlook.Unity.Pathfinding
         }
 
         [Header("Obstacle Avoidance")]
-        [SerializeField, Tooltip("Determines which layers does the agent tries to avoid.")]
-        public LayerMask avoidanceLayers;
-
-        [SerializeField, Min(0), Tooltip("Determines the radius in which tries to avoid obstacles.")]
-        private float avoidanceRadius = 1.5f;
-        public float AvoidanceRadius {
-            get => avoidanceRadius;
-            set => avoidanceRadius = ErrorMessage.NoNegativeGuard(nameof(AvoidanceRadius), value);
-        }
-
-        [SerializeField, Min(0), Tooltip("Determines the avoidance strength to repel from obstacles.")]
-        private float avoidanceStrength = 1;
-        public float AvoidanceStrength {
-            get => avoidanceStrength;
-            set => avoidanceStrength = ErrorMessage.NoNegativeGuard(nameof(AvoidanceStrength), value);
-        }
-
-        [SerializeField, Min(0), Tooltip("Determines the prediction time used for moving obstacles to avoid their futures positions.")]
-        private float avoidancePredictionTime = 1;
-        public float AvoidancePredictionTime {
-            get => avoidancePredictionTime;
-            set => avoidancePredictionTime = ErrorMessage.NoNegativeGuard(nameof(AvoidancePredictionTime), value);
-        }
-
-        [SerializeField, Min(0), ShowIf(nameof(avoidancePredictionTime), typeof(float), 0, mustBeEqual: false), Tooltip("Determines additional detection radius used for moving obstacles.")]
-        private float avoidancePredictionRadius = 1;
-        public float AvoidancePredictionRadius {
-            get => avoidancePredictionRadius;
-            set => avoidancePredictionRadius = ErrorMessage.NoNegativeGuard(nameof(AvoidancePredictionRadius), value);
-        }
+        [SerializeField, Tooltip("Configuration of the obstacle avoidance.")]
+        public ObstacleAvoidance obstacleAvoidance;
 
         internal Rigidbody Rigidbody;
 
         private DynamicArray<Vector3> innerPath;
         internal DynamicArray<Vector3>.Enumerator enumerator;
         private bool hasPath;
-
-        private static Collider[] colliders = new Collider[100];
-        private static int COLLIDER_GROW_FACTOR = 2;
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void Awake()
@@ -108,8 +74,8 @@ namespace Enderlook.Unity.Pathfinding
                 return;
 
             Vector3 movementDirection = CalculateCurrentDirection();
-            Vector3 avoidDirection = CalculateObstacleAvoidance();
-            Vector3 direction = movementDirection + avoidDirection * avoidanceStrength;
+            Vector3 avoidDirection = obstacleAvoidance.GetDirection(Rigidbody);
+            Vector3 direction = movementDirection + avoidDirection;
             MoveAndRotate(direction);
 
 #if UNITY_EDITOR
@@ -138,75 +104,6 @@ namespace Enderlook.Unity.Pathfinding
             }
 
             return direction.normalized;
-        }
-
-        private Vector3 CalculateObstacleAvoidance()
-        {
-            float radius = avoidanceRadius;
-            if (avoidancePredictionTime != 0)
-                radius += avoidancePredictionRadius;
-
-            start:
-            int amount = Physics.OverlapSphereNonAlloc(Rigidbody.position, radius, colliders, avoidanceLayers);
-            if (amount == colliders.Length)
-            {
-                Array.Resize(ref colliders, colliders.Length * COLLIDER_GROW_FACTOR);
-                goto start;
-            }
-
-            if (amount == 0)
-                return Vector3.zero;
-
-            Span<Collider> span = colliders.AsSpan(0, amount);
-            Vector3 currentPosition = Rigidbody.position;
-            Vector3 total = Vector3.zero;
-            int count = 0;
-            foreach (Collider collider in span)
-            {
-                if (IsMine(collider.transform))
-                    continue;
-
-                Vector3 position;
-                Vector3 closestPoint = collider.ClosestPointOnBounds(Rigidbody.position);
-                if (collider.TryGetComponent(out Rigidbody _rigidbody))
-                    position = closestPoint + _rigidbody.velocity * avoidancePredictionTime;
-                else
-                    position = closestPoint;
-
-                Vector3 direction = position - currentPosition;
-                float distance = direction.magnitude;
-                if (distance >= avoidanceRadius)
-                    continue;
-
-                count++;
-                total -= (avoidanceRadius - distance) / avoidanceRadius * direction;
-            }
-
-            if (count == 0)
-                return Vector3.zero;
-
-            total /= count;
-
-            if (total.sqrMagnitude > 1)
-                total = total.normalized;
-
-            return total;
-        }
-
-        private bool IsMine(Transform transform)
-        {
-            if (transform == this.transform)
-                return true;
-
-            transform = transform.parent;
-            while (transform != null)
-            {
-                if (transform == this.transform)
-                    return true;
-                transform = transform.parent;
-            }
-
-            return false;
         }
 
         private void MoveAndRotate(Vector3 direction)
@@ -241,12 +138,12 @@ namespace Enderlook.Unity.Pathfinding
                 Rigidbody = GetComponent<Rigidbody>();
 
             Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(Rigidbody.position, avoidanceRadius);
+            Gizmos.DrawWireSphere(Rigidbody.position, obstacleAvoidance.AvoidanceRadius);
 
             Color orange = (Color.yellow + Color.red) / 2;
             orange.a = 1;
             Gizmos.color = orange;
-            Gizmos.DrawWireSphere(Rigidbody.position, avoidanceRadius + avoidancePredictionRadius);
+            Gizmos.DrawWireSphere(Rigidbody.position, obstacleAvoidance.AvoidanceRadius + obstacleAvoidance.AvoidancePredictionRadius);
 
             Gizmos.color = Color.cyan;
             Gizmos.DrawLine(Rigidbody.position, Rigidbody.position + Rigidbody.velocity);
