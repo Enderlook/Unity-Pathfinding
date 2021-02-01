086f074cb7a9f57941520893e8c5ee6edc03f830
Enderlook <enderlook@gmail.com>
2021-02-01 16:36:58 -0300

    Convert into package

---
+++ Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using Enderlook.Unity.Pathfinding;
+
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[DefaultExecutionOrder(100)]
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private NavigationAgent agent;
+    [SerializeField]
+    private NavigationAgent agent2;
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Collider plane;
+
+    //[System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+    //private void Awake() => agent.SetDestinationSync(target.position);
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown((int)MouseButton.LeftMouse))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider == plane)
+            {
+                agent.SetDestination(hit.point);
+                agent2.SetDestination(hit.point);
+            }
+        }
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+    private void OnDrawGizmos()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(hit.point, 1);
+        }
+    }
+}
---
+++ Packages/com.enderlook.unity.pathfinding/Editor/NavigationAgentEditor.cs
@@ -0,0 +1,55 @@
+﻿using Enderlook.Unity.Toolset.Utils;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Pathfinding
+{
+    [CustomEditor(typeof(NavigationAgent))]
+    internal sealed class NavigationAgentEditor : Editor
+    {
+        private static readonly GUIContent DRAW_PATH_TOGGLE = new GUIContent("Draw Path", "Draw the current set path.");
+
+        private new NavigationAgent target;
+
+        private bool drawPath;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void OnEnable() => target = (NavigationAgent)base.target;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            EditorGUILayout.Space();
+            EditorGUIHelper.Header("Other");
+
+            drawPath = EditorGUILayout.Toggle(DRAW_PATH_TOGGLE, drawPath);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void OnSceneGUI()
+        {
+            if (drawPath)
+            {
+                DynamicArray<Vector3>.Enumerator enumerator = target.PathFollower.enumerator;
+                if (enumerator.IsDefault)
+                    return;
+
+                Handles.color = Color.blue;
+                Vector3 start;
+                Vector3 end = target.transform.position;
+                enumerator.MoveBack();
+                while (enumerator.MoveNext())
+                {
+                    Handles.DrawWireCube(end, Vector3.one * .1f);
+                    start = end;
+                    end = enumerator.Current;
+                    Handles.DrawLine(start, end);
+                }
+                Handles.DrawWireCube(end, Vector3.one * .1f);
+            }
+        }
+    }
+}
