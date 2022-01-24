9c065dfb7a1268fb997e93fe35ff4c58f729a6fe
Enderlook <enderlook@gmail.com>
2022-01-24 16:38:22 -0300

    Refactor IGraphLineOfSight

--- Packages/com.enderlook.unity.pathfinding/Runtime/Graph/IGraphLineOfSight.cs
+++ Packages/com.enderlook.unity.pathfinding/Runtime/Graph/IGraphLineOfSight.cs
@@ -1,24 +1,24 @@
 ﻿namespace Enderlook.Unity.Pathfinding
 {
     /// <summary>
-    /// Interface to check if two coordinates has line of sight in a graph.
+    /// Interface to check if two points has line of sight in a graph.
     /// </summary>
-    /// <typeparam name="TCoord">Type of coordinate.</typeparam>
-    internal interface IGraphLineOfSight<TCoord>
+    /// <typeparam name="T">Type of point.</typeparam>
+    internal interface IGraphLineOfSight<T>
     {
         /// <summary>
-        /// <see langword="true"/> if <see cref="HasLineOfSight(TCoord, TCoord)"/> can only be executed from Unity thread.<br/>
+        /// <see langword="true"/> if <see cref="HasLineOfSight(T, T)"/> can only be executed from Unity thread.<br/>
         /// Otherwise it can be executed from other threads.<br/>
         /// If this is not respected, undefined behaviour.
         /// </summary>
         bool RequiresUnityThread { get; }
 
         /// <summary>
-        /// Determines if both coordinates has line of sight.
+        /// Determines if both points has line of sight.
         /// </summary>
-        /// <param name="from">Start coodinate.</param>
-        /// <param name="to">End coordinate.</param>
+        /// <param name="from">Start points.</param>
+        /// <param name="to">End points.</param>
         /// <returns>Whenever they have line of sight or not.</returns>
-        bool HasLineOfSight(TCoord from, TCoord to);
+        bool HasLineOfSight(T from, T to);
     }
 }
