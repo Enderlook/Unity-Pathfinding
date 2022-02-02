ff2f30edd6a0b65b9801c5481743febe9eeef20d
Enderlook <enderlook@gmail.com>
2022-02-02 17:02:40 -0300

    Remove unused using

--- Packages/com.enderlook.unity.pathfinding/Runtime/Navigation Surface/Generation/Voxelizer-VoxelizeMesh.cs
+++ Packages/com.enderlook.unity.pathfinding/Runtime/Navigation Surface/Generation/Voxelizer-VoxelizeMesh.cs
@@ -6,7 +6,6 @@ using System;
 using System.Diagnostics;
 using System.Numerics;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 using Mathf = UnityEngine.Mathf;
