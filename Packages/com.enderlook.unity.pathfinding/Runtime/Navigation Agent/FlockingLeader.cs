c80138899ab1e94276e4f77a9f1f58ab6a612836
Enderlook <enderlook@gmail.com>
2022-03-28 10:44:32 -0300

    Dispose pools in FlockingLeader when is destroyed

--- Packages/com.enderlook.unity.pathfinding/Runtime/Navigation Agent/FlockingLeader.cs
+++ Packages/com.enderlook.unity.pathfinding/Runtime/Navigation Agent/FlockingLeader.cs
@@ -38,6 +38,16 @@ namespace Enderlook.Unity.Pathfinding
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void Awake() => Rigidbody = GetComponent<Rigidbody>();
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void OnDestroy()
+        {
+            followers.Clear();
+            followers.Dispose();
+            followersInRange.Dispose();
+            ArrayPool<Vector3>.Shared.Return(followersPositions);
+            followersPositions = null;
+        }
+
         internal void AddFollower(FlockingFollower follower)
         {
             followers.Add(follower.Rigidbody);
