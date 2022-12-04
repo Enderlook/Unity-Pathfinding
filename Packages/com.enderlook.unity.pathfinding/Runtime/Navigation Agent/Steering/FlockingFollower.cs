98565fa1251d6fb74664080ba1411be71c513f9a
Enderlook <enderlook@gmail.com>
2022-12-04 10:32:12 -0300

    Fix FlockingFollower no following leader

--- Packages/com.enderlook.unity.pathfinding/Runtime/Navigation Agent/Steering/FlockingFollower.cs
+++ Packages/com.enderlook.unity.pathfinding/Runtime/Navigation Agent/Steering/FlockingFollower.cs
@@ -148,7 +148,7 @@ namespace Enderlook.Unity.Pathfinding.Steerings
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void OnEnable()
         {
-            if (!subscribedToLeader)
+            if (subscribedToLeader)
                 return;
             FlockingLeader.AddFollower(this);
             subscribedToLeader = true;
